eeadad2b403793cc6f37bfd62e4539eead84d049
E B Benson <117281795+Deriverx2@users.noreply.github.com>
2023-08-09 11:12:07 +0530

    Update FinalizeDemo.java

--- lab_cycle_1/src/Que10/FinalizeDemo.java
+++ lab_cycle_1/src/Que10/FinalizeDemo.java
@@ -1,4 +1,17 @@
 package Que10;
 
 class FinalizeDemo {
+  int a,b,c;
+    ABC(int a,int b,int c){
+        this.a=a;
+        this.b=b;
+        this.c=c;
+    }
+    public void printProduct(){
+        System.out.println("product="+(a+b+c));
+    }
+    public void finalize(){
+        System.out.println("garbageCollected ");
+    }
+    
 }
