8307f62b59fd1eb93ad4e18c2bb4f590c3e2c61a
E B Benson <117281795+Deriverx2@users.noreply.github.com>
2023-08-09 10:28:10 +0530

    Create TowerOfHanoi.java

---
+++ lab_cycle_2/src/Que11/TowerOfHanoi.java
@@ -0,0 +1,24 @@
+package Que11;
+import java.util.Scanner;
+/**
+ *
+ * @author nazal
+ */
+public class TowerOfHanoi {
+    static void hanoi(String start, String end, String middle, int n){
+        if(n==0){
+            return;
+        }
+        hanoi(start,middle,end,n-1);
+        System.out.print("Move disk " + n + " from " + start + " to " +end +"\n");
+        hanoi(middle,end,start,n-1);
+    }
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no of disks : ");
+        int n = sc.nextInt();
+        hanoi("A","C","B",n);
+        
+    }
+}
