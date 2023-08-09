b26775bd43313212c4436610441051ed4ecb0a65
E B Benson <117281795+Deriverx2@users.noreply.github.com>
2023-08-09 10:35:24 +0530

    Create EmployeeMain.java

---
+++ lab_cycle_2/src/Que14/EmployeeMain.java
@@ -0,0 +1,12 @@
+
+package Que14;
+
+
+public class EmployeeMain {
+    public static void main(String[] args){
+        EmployeeList obj = new EmployeeList(2);
+        obj.getEmployees();
+        obj.sortList();
+        obj.displaySorted();
+    }
+}
