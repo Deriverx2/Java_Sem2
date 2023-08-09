85d2516e5051a4e60e312a9296063be8e0598466
E B Benson <117281795+Deriverx2@users.noreply.github.com>
2023-08-09 10:42:23 +0530

    Update ExceptionHandler.java

--- lab_cycle_2/src/Que17/ExceptionHandler.java
+++ lab_cycle_2/src/Que17/ExceptionHandler.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ExceptionHandlerForms/JFrame.java to edit this template
- */
+
 package q7;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author nazal
- */
+
 public class ExceptionHandler extends javax.swing.JFrame {
 
     /**
